a9089446a93b800777e23f92c0bc66f0128bec35
AlmasShintemirov <ashintemirov@gmail.com>
2021-07-22 09:39:50 +0600

    TODO 1 and 2

--- src/main.cpp
+++ src/main.cpp
@@ -50,11 +50,16 @@ int main(int argc, const char **argv)
             std::cout << "Failed to read." << std::endl;
         else
             osm_data = std::move(*data);
-    }
+    } 
     
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter x and y coordinates of the start point\n";
+    std::cin >> start_x >> start_y;
+    std::cout << "Enter x and y coordinates of the end point\n";
+    std::cin >> end_x >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
